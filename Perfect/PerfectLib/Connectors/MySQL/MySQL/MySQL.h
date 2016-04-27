189ab9105a2e5bfb228f692dfdd071edbc268d94
chp <lrw90316@163.com>
2016-04-27 16:49:04 +0800

    mysql

---
+++ Perfect/PerfectLib/Connectors/MySQL/MySQL/MySQL.h
@@ -0,0 +1,19 @@
+//
+//  MySQL.h
+//  MySQL
+//
+//  Created by Riven on 16/4/26.
+//  Copyright © 2016年 Riven. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MySQL.
+FOUNDATION_EXPORT double MySQLVersionNumber;
+
+//! Project version string for MySQL.
+FOUNDATION_EXPORT const unsigned char MySQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySQL/PublicHeader.h>
+
+
