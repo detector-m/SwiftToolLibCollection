1801f2fd401af8b909c111547bd4152f67c52026
chp <lrw90316@163.com>
2016-04-29 12:08:11 +0800

    add 13

--- Perfect/PerfectLib/Connectors/Mod_perfect/Mod_perfect/Mod_perfect.hpp
+++ Perfect/PerfectLib/Connectors/Mod_perfect/Mod_perfect/Mod_perfect.hpp
@@ -10,8 +10,20 @@
 #ifndef Mod_perfect_
 #define Mod_perfect_
 
+#include "httpd.h"
+#ifdef strtoul
+#undef strtoul
+#endif
+#include <http_config.h>
+#include <http_protocol.h>
+#include <http_protocol.h>
+#include <http_main.h>
+#include <http_core.h>
+#include <ap_compat.h>
+
 /* The classes below are exported */
 #pragma GCC visibility push(default)
+
 
 class Mod_perfect
 {
